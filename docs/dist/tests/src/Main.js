93ef55a815f5a161118ceae9b58ce237b7504482
Akash Mair <akash@compositional-it.com>
2021-05-17 20:56:49 +0100

    updated title

--- docs/dist/tests/src/Main.js
+++
@@ -1,8 +0,0 @@
-import { render } from "react-dom";
-import { createElement } from "react";
-import { Components_HelloWorld } from "./Components.js";
-import "../../../src/styles/global.scss";
-
-
-render(createElement(Components_HelloWorld, null), document.getElementById("feliz-app"));
-
