da109b65477c3a9cfbef7ebd5a1a13958f2af875
Akash Mair <akash@compositional-it.com>
2021-05-17 20:49:27 +0100

    updated publish script

--- docs/publish.js
+++ docs/publish.js
@@ -2,7 +2,7 @@ var ghPages = require("gh-pages");
 
 
 // Update with wrapper repo
-var packageUrl = "https://github.com/CompositionalIT/feliz-________.git";
+var packageUrl = "https://github.com/CompositionalIT/feliz-ag-grid.git";
 
 console.log("Publishing to ", packageUrl);
 
