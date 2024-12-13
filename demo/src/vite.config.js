01c493ffe9411d9dff88d95f8e6d1cfd3d414845
Joost Kaptein <joost@compositional-it.com>
2024-12-13 15:08:55 +0000

    set up for vite

---
+++ demo/src/vite.config.js
@@ -0,0 +1,10 @@
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fable from "vite-plugin-fable"
+
+export default defineConfig({
+    plugins: [react(), fable()],
+    server: {
+        port: 8080,
+    }
+})
