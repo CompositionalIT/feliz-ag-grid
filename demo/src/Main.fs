5c46c741f2e2632364cb2905d126006e2885b3f6
Joost Kaptein <joost@compositional-it.com>
2025-01-17 13:55:43 +0000

    Use a Elmish app and memoize the columns, so data changes do not discard grid changes

--- demo/src/Main.fs
+++ demo/src/Main.fs
@@ -3,9 +3,16 @@ module Main
 open Feliz
 open Browser.Dom
 open Fable.Core.JsInterop
+open Elmish
+open Elmish.React
 
 importSideEffects "./styles/global.scss"
 
-let root = ReactDOM.createRoot (document.getElementById "feliz-app")
 
-root.render (App.Documentation())
+Program.mkProgram App.init App.update App.Documentation
+//-:cnd:noEmit
+#if DEBUG
+|> Program.withConsoleTrace
+#endif
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
